e80d2e09d7dc373d5591faa8862d383591523e9f
Kossie Coder <kossie.coder@gmail.com>
2020-12-24 12:25:55 +1000

    how to use function

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
-  <div class="name">{{ name }}</div>
-  <div>Hi</div>
+  <div class="name">
+    {{ greet }}
+  </div>
 </template>
 
 <script>
@@ -8,8 +9,14 @@ export default {
   setup() {
     const name = 'Kossie Coder1';
 
+    const greeting = (name) => {
+      return 'Hello, ' + name;
+    };
+
+    const greet = greeting(name); 
+
     return {
-      name
+      greet
     };
   }
 }

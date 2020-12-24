daecb9128b6f61a56647fc561991f1194bdfa428
Kossie Coder <kossie.coder@gmail.com>
2020-12-24 10:38:42 +1000

    add script and style to App component

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,21 @@
 <template>
-  <div>Kossie coder</div>
+  <div class="name">{{ name }}</div>
 </template>
+
+<script>
+export default {
+  setup() {
+    const name = 'Kossie Coder1';
+
+    return {
+      name
+    };
+  }
+}
+</script>
+
+<style>
+  .name {
+    color: red;
+  }
+</style>

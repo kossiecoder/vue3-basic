fba00c07229351e116a6bfe24811a57675582fc8
Kossie Coder <kossie.coder@gmail.com>
2020-12-29 22:17:05 +1000

    11

--- src/App.vue
+++ src/App.vue
@@ -22,10 +22,17 @@
         </button>
       </div>
     </form>
-    {{ todos }}
+    <div class="card mt-2">
+      <div class="card-body p-2">
+        {{ todos[0].subject }}
+      </div>
+    </div>
+    <div class="card mt-2">
+      <div class="card-body p-2">
+        {{ todos[1].subject }}
+      </div>
+    </div>
   </div>
-  
-  
 </template>
 
 <script>
@@ -34,7 +41,10 @@ import { ref } from 'vue';
 export default {
   setup() {
     const todo = ref('');
-    const todos = ref([]);
+    const todos = ref([
+      {id: 1, subject: '휴대폰 사기'},
+      {id: 2, subject: '장보기'},
+    ]);
 
     const onSubmit = () => {
       todos.value.push({

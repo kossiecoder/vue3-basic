579ac2ffceb80b7681fac46c8757c74544724323
Kossie Coder <kossie.coder@gmail.com>
2020-12-30 10:56:31 +1000

    12

--- src/App.vue
+++ src/App.vue
@@ -22,14 +22,13 @@
         </button>
       </div>
     </form>
-    <div class="card mt-2">
+    <div 
+      v-for="todo in todos"
+      :key="todo.id"
+      class="card mt-2"
+    >
       <div class="card-body p-2">
-        {{ todos[0].subject }}
-      </div>
-    </div>
-    <div class="card mt-2">
-      <div class="card-body p-2">
-        {{ todos[1].subject }}
+        {{ todo.subject }}
       </div>
     </div>
   </div>

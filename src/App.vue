2353373ccf0795b977966dff9bf9c4c999e3e9a2
Kossie Coder <kossie.coder@gmail.com>
2020-12-31 11:16:56 +1000

    17

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,8 @@
 <template>
   <div class="container">
     <h2>To-Do List</h2>
-    <form @submit.prevent="onSubmit">
-      <div class="d-flex">
-        <div class="flex-grow-1 mr-2">
-          <input
-            class="form-control"
-            type="text" 
-            v-model="todo"
-            placeholder="Type new to-do"
-          >
-        </div>
-        <div>
-          <button 
-            class="btn btn-primary"
-            type="submit"
-          >
-            Add
-          </button>
-        </div>
-      </div>
-      <div v-show="hasError" style="color: red">
-        This field cannot be empty
-      </div>
-    </form>
+    <TodoSimpleForm />
+    
     <div v-if="!todos.length">
       추가된 Todo가 없습니다
     </div>
@@ -61,8 +40,12 @@
 
 <script>
 import { ref } from 'vue';
+import TodoSimpleForm from './components/TodoSimpleForm.vue';
 
 export default {
+  components: {
+    TodoSimpleForm
+  },
   setup() {
     const todo = ref('');
     const todos = ref([]);
---
+++ src/components/TodoSimpleForm.vue
@@ -0,0 +1,31 @@
+<template>
+    <form @submit.prevent="onSubmit">
+        <div class="d-flex">
+        <div class="flex-grow-1 mr-2">
+            <input
+            class="form-control"
+            type="text" 
+            v-model="todo"
+            placeholder="Type new to-do"
+            >
+        </div>
+        <div>
+            <button 
+            class="btn btn-primary"
+            type="submit"
+            >
+            Add
+            </button>
+        </div>
+        </div>
+        <div v-show="hasError" style="color: red">
+        This field cannot be empty
+        </div>
+    </form>
+</template>
+
+<script>
+    export default {}
+</script>
+
+<style></style>

aa981815cecbe45732a6076224f77dce847cd9f2
Kossie Coder <kossie.coder@gmail.com>
2020-12-31 15:08:22 +1000

    19

--- src/App.vue
+++ src/App.vue
@@ -6,48 +6,21 @@
     <div v-if="!todos.length">
       추가된 Todo가 없습니다
     </div>
-    <div 
-      v-for="(todo, index) in todos"
-      :key="todo.id"
-      class="card mt-2"
-    >
-      <div class="card-body p-2 d-flex align-items-center">
-        <div class="form-check flex-grow-1">
-          <input 
-            class="form-check-input" 
-            type="checkbox"
-            v-model="todo.completed"
-          >
-          <label 
-            class="form-check-label"
-            :class="{ todo: todo.completed }"
-          >
-            {{ todo.subject }}
-          </label>
-        </div>
-        <div>
-          <button 
-            class="btn btn-danger btn-sm"
-            @click="deleteTodo(index)"
-          >
-            Delete
-          </button>
-        </div>
-      </div>
-    </div>
+    <TodoList :todos="todos" />
   </div>
 </template>
 
 <script>
 import { ref } from 'vue';
 import TodoSimpleForm from './components/TodoSimpleForm.vue';
+import TodoList from './components/TodoList.vue';
 
 export default {
   components: {
-    TodoSimpleForm
+    TodoSimpleForm,
+    TodoList,
   },
   setup() {
-    
     const todos = ref([]);
 
     const addTodo = (todo) => {
---
+++ src/components/TodoList.vue
@@ -0,0 +1,46 @@
+<template>
+  <div 
+      v-for="(todo, index) in todos"
+      :key="todo.id"
+      class="card mt-2"
+    >
+      <div class="card-body p-2 d-flex align-items-center">
+        <div class="form-check flex-grow-1">
+          <input 
+            class="form-check-input" 
+            type="checkbox"
+            v-model="todo.completed"
+          >
+          <label 
+            class="form-check-label"
+            :class="{ todo: todo.completed }"
+          >
+            {{ todo.subject }}
+          </label>
+        </div>
+        <div>
+          <button 
+            class="btn btn-danger btn-sm"
+            @click="deleteTodo(index)"
+          >
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        todos: {
+            type: Array,
+            required: true
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>

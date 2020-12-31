cbe1db7429a2e67e991c28f3db816476936b3780
Kossie Coder <kossie.coder@gmail.com>
2020-12-31 12:12:13 +1000

    18

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <h2>To-Do List</h2>
-    <TodoSimpleForm />
+    <TodoSimpleForm @add-todo="addTodo" />
     
     <div v-if="!todos.length">
       추가된 Todo가 없습니다
@@ -47,22 +47,11 @@ export default {
     TodoSimpleForm
   },
   setup() {
-    const todo = ref('');
+    
     const todos = ref([]);
-    const hasError = ref(false);
 
-    const onSubmit = () => {
-      if (todo.value === '') {
-        hasError.value = true;
-      } else {
-        todos.value.push({
-          id: Date.now(),
-          subject: todo.value,
-          completed: false,
-        });
-        hasError.value = false;
-        todo.value = '';
-      }
+    const addTodo = (todo) => {
+      todos.value.push(todo);
     };
 
     const deleteTodo = (index) => {
@@ -70,10 +59,8 @@ export default {
     };
 
     return {
-      todo,
       todos,
-      onSubmit,
-      hasError,
+      addTodo,
       deleteTodo,
     };
   }
--- src/components/TodoSimpleForm.vue
+++ src/components/TodoSimpleForm.vue
@@ -1,31 +1,57 @@
 <template>
     <form @submit.prevent="onSubmit">
         <div class="d-flex">
-        <div class="flex-grow-1 mr-2">
-            <input
-            class="form-control"
-            type="text" 
-            v-model="todo"
-            placeholder="Type new to-do"
-            >
-        </div>
-        <div>
-            <button 
-            class="btn btn-primary"
-            type="submit"
-            >
-            Add
-            </button>
-        </div>
+            <div class="flex-grow-1 mr-2">
+                <input
+                    class="form-control"
+                    type="text" 
+                    v-model="todo"
+                    placeholder="Type new to-do"
+                >
+            </div>
+            <div>
+                <button 
+                    class="btn btn-primary"
+                    type="submit"
+                >
+                    Add
+                </button>
+            </div>
         </div>
         <div v-show="hasError" style="color: red">
-        This field cannot be empty
+            This field cannot be empty
         </div>
     </form>
 </template>
 
 <script>
-    export default {}
+    import { ref } from 'vue';
+
+    export default {
+        setup(props, context) {
+            const todo = ref('');
+            const hasError = ref(false);
+            const onSubmit = () => {
+                if (todo.value === '') {
+                    hasError.value = true;
+                } else {
+                    context.emit('add-todo', {
+                        id: Date.now(),
+                        subject: todo.value,
+                        completed: false,
+                    });
+                    hasError.value = false;
+                    todo.value = '';
+                }
+            };
+
+            return {
+                todo,
+                hasError,
+                onSubmit,
+            };
+        }
+    }
 </script>
 
 <style></style>

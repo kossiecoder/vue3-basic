8874709a27cc53d2cf0c3854da8d2aa60a7fca8f
Kossie Coder <kossie.coder@gmail.com>
2021-02-19 23:13:35 +1000

    33

--- src/App.vue
+++ src/App.vue
@@ -22,25 +22,25 @@
     <hr />
     <nav aria-label="Page navigation example">
       <ul class="pagination">
-        <li class="page-item">
+        <li v-if="currentPage !== 1" class="page-item">
           <a class="page-link" href="#">
             Previous
           </a>
         </li>
-        <li class="page-item">
-          <a class="page-link" href="#">1</a>
+        <li
+          v-for="page in numberOfPages"
+          :key="page" 
+          class="page-item"
+          :class="currentPage === page ? 'active' : ''"
+        >
+          <a class="page-link" href="#">{{page}}</a>
         </li>
-        <li class="page-item">
-          <a class="page-link" href="#">2</a>
-        </li>
-        <li class="page-item">
-          <a class="page-link" href="#">3</a>
-        </li>
-        <li class="page-item">
+        <li v-if="numberOfPages !== currentPage" class="page-item">
           <a class="page-link" href="#">Next</a>
         </li>
       </ul>
     </nav>
+    {{numberOfPages}}
   </div>
 </template>
 
@@ -58,16 +58,20 @@ export default {
   setup() {
     const todos = ref([]);
     const error = ref('');
-    const totalPage = ref(0);
+    const numberOfTodos = ref(0);
     const limit = 5;
-    const page = ref(1);
+    const currentPage = ref(3);
+
+    const numberOfPages = computed(() => {
+      return Math.ceil(numberOfTodos.value/limit);
+    });
 
     const getTodos = async () => {
       try {
         const res = await axios.get(
-          `http://localhost:3000/todos?_page=${page.value}&_limit=${limit}`
+          `http://localhost:3000/todos?_page=${currentPage.value}&_limit=${limit}`
         );
-        totalPage.value = res.headers['x-total-count'];
+        numberOfTodos.value = res.headers['x-total-count'];
         todos.value = res.data;
       } catch (err) {
         console.log(err);
@@ -139,6 +143,8 @@ export default {
       searchText,
       filteredTodos,
       error,
+      numberOfPages,
+      currentPage,
     };
   }
 }

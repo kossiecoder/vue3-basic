12214d0374565e6c4920ff8a16838a3afbc83822
Kossie Coder <kossie.coder@gmail.com>
2021-03-04 16:07:28 +1000

    43

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Kossie Coder</a>
+  <router-link class="navbar-brand" to="/">
+    Kossie Coder
+  </router-link>
 
   <ul class="navbar-nav mr-auto">
     <li class="nav-item active">
-      <a class="nav-link" href="#">Todos <span class="sr-only">(current)</span></a>
+      <router-link class="nav-link" to="/todos">
+        Todos
+      </router-link>
     </li>
   </ul>
 </nav>

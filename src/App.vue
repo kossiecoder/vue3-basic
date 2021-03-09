b34a42636c62f599018f0e79c9a4b57315861637
Kossie Coder <kossie.coder@gmail.com>
2021-03-09 20:43:35 +1000

    46

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <router-link class="navbar-brand" to="/">
+  <router-link class="navbar-brand" :to="{ name: 'Home'}">
     Kossie Coder
   </router-link>
 
   <ul class="navbar-nav mr-auto">
     <li class="nav-item active">
-      <router-link class="nav-link" to="/todos">
+      <router-link class="nav-link" :to="{ name: 'Todos'}">
         Todos
       </router-link>
     </li>
--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -4,7 +4,10 @@
       :key="todo.id"
       class="card mt-2"
     >
-      <div class="card-body p-2 d-flex align-items-center">
+      <div 
+        class="card-body p-2 d-flex align-items-center"
+        @click="moveToPage(todo.id)"
+      >
         <div class="form-check flex-grow-1">
           <input 
             class="form-check-input" 
@@ -32,6 +35,8 @@
 </template>
 
 <script>
+import { useRouter } from 'vue-router';
+
 export default {
     props: {
         todos: {
@@ -41,6 +46,7 @@ export default {
     },
     emits: ['toggle-todo', 'delete-todo'],
     setup(props, { emit }) {
+        const router = useRouter();
         const toggleTodo = (index) => {
           emit('toggle-todo', index);
         };
@@ -49,9 +55,21 @@ export default {
           emit('delete-todo', index);
         };
 
+        const moveToPage = (todoId) => {
+          // router.push('/todos/' + todoId);
+          router.push({
+            name: 'Todo',
+            params: {
+              id: todoId
+            }
+          });
+
+        };
+
         return {
             toggleTodo,
             deleteTodo,
+            moveToPage,
         };
     }
 }

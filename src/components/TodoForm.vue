8d2749758df48ab6b518e7065265e38bf9f8f6e7
Kossie Coder <kossie.coder@gmail.com>
2021-03-19 22:24:49 +1000

    62

--- src/components/TodoForm.vue
+++ src/components/TodoForm.vue
@@ -60,11 +60,13 @@
       Cancel
     </button>
   </form>
-  <Toast 
-    v-if="showToast" 
-    :message="toastMessage"
-    :type="toastAlertType"
-  />
+  <transition name="fade">
+    <Toast 
+      v-if="showToast" 
+      :message="toastMessage"
+      :type="toastAlertType"
+    />
+  </transition>
 </template>
 
 <script>
@@ -195,5 +197,22 @@ export default {
 <style scoped>
   .text-red {
     color: red;
+  }
+
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: all 0.5s ease;
+  }
+
+  .fade-enter-from,
+  .fade-leave-to {
+    opacity: 0;
+    transform: translateY(-30px);
+  }
+
+  .fade-enter-to,
+  .fade-leave-from {
+    opacity: 1;
+    transform: translateY(0px);
   }
 </style>

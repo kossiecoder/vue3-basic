7bc3e1170121688e8ed63b3c0c3bcacd0a431cf8
Kossie Coder <kossie.coder@gmail.com>
2021-03-20 22:03:33 +1000

    63

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -9,20 +9,17 @@
         style="cursor: pointer"
         @click="moveToPage(todo.id)"
       >
-        <div class="form-check flex-grow-1">
+        <div class="flex-grow-1">
           <input 
-            class="form-check-input" 
+            class="ml-2 mr-2"
             type="checkbox"
             :checked="todo.completed"
             @change="toggleTodo(index, $event)"
             @click.stop
           >
-          <label 
-            class="form-check-label"
-            :class="{ todo: todo.completed }"
-          >
+          <span :class="{ todo: todo.completed }">
             {{ todo.subject }}
-          </label>
+          </span>
         </div>
         <div>
           <button 

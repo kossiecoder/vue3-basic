b98be7977672aae9255cf28644f0300d623c8f92
Kossie Coder <kossie.coder@gmail.com>
2021-03-24 21:16:36 +1000

    71

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -42,7 +42,7 @@
 <script>
 import { useRouter } from 'vue-router';
 import Modal from '@/components/DeleteModal.vue';
-import { ref } from 'vue';
+import { ref, useContext } from 'vue';
 import List from '@/components/List.vue';
 
 export default {
@@ -57,7 +57,8 @@ export default {
         }
     },
     emits: ['toggle-todo', 'delete-todo'],
-    setup(props, { emit }) {
+    setup() {
+        const { emit } = useContext();
         const router = useRouter();
         const showModal = ref(false);
         const todoDeleteId = ref(null);

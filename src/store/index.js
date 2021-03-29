f8677b4f6e05aba06dd067923415ff37996dd4d8
Kossie Coder <kossie.coder@gmail.com>
2021-03-29 21:45:23 +1000

    77

--- src/store/index.js
+++ src/store/index.js
@@ -6,5 +6,40 @@ export default createStore({
         toastAlertType: '',
         showToast: false, 
         timeout: null
+    },
+    mutations: {
+        UPDATE_TOAST_MESSAGE (state, payload) {
+            state.toastMessage = payload;
+        },
+        UPDATE_TOAST_ALERT_TYPE (state, payload) {
+            state.toastAlertType = payload;
+        },
+        UPDATE_TOAST_STATUS (state, payload) {
+            state.showToast = payload;
+        },
+        UPDATE_TOAST_TIMEOUT (state, payload) {
+            state.timeout = payload;
+        }
+    },
+    actions: {
+        triggerToast({ commit }, message, type = 'success') {
+            // toastMessage.value = message;
+            commit('UPDATE_TOAST_MESSAGE', message)
+            // toastAlertType.value = type;
+            commit('UPDATE_TOAST_ALERT_TYPE', type)
+            // showToast.value = true;
+            commit('UPDATE_TOAST_STATUS', true)
+
+            const timeout = setTimeout(() => {
+            //   toastMessage.value = '';
+              commit('UPDATE_TOAST_MESSAGE', '')
+            //   toastAlertType.value = '';
+              commit('UPDATE_TOAST_ALERT_TYPE', '')
+            //   showToast.value = false;
+              commit('UPDATE_TOAST_STATUS', false)
+            }, 5000)
+
+            commit('UPDATE_TOAST_TIMEOUT', timeout);
+          }
     }
 });
